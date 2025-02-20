82de08a0b1e586a29306cd100adac50866d07003
Viacheslav Saprykin <saprykinvp@gmail.com>
2025-02-20 17:05:13 +0200

    MA_Less1_ver.2 - strilochna function

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,21 @@ import { createRoot } from 'react-dom/client'
 
 const element = document.querySelector('#root');
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>New text</h1>
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>New text</h1>
+//     </div>
+//   );
+// }
 createRoot(element).render(
   <App />
 );
