b6cf894220c4caaae50b81bfc87818714891929c
Viacheslav Saprykin <saprykinvp@gmail.com>
2025-02-20 17:15:53 +0200

    MA_Less1_ver.3 - export&import component

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,8 @@
 import { createRoot } from 'react-dom/client'
+import { App } from './App'
 
 const element = document.querySelector('#root');
 
-const App = () => {
-  return (
-    <div>
-      <h1>New text</h1>
-    </div>
-  );
-}
-
-// function App() {
-//   return (
-//     <div>
-//       <h1>New text</h1>
-//     </div>
-//   );
-// }
 createRoot(element).render(
   <App />
 );
