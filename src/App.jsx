499479d1ce74ebc011ff4a8c76c0f15a05aebdf7
Viacheslav Saprykin <saprykinvp@gmail.com>
2025-02-21 18:54:26 +0200

    MA_Less1_ver.8 - Rander list

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
-import { Card } from "./components/Product/Product";
-import "./App.scss";
-import './components/Product/Product.scss';
-
+import goods from "./goods";
 
 export const App = () => (
-  <div className="App">
-    <h1>My Shop</h1>
-    <Card
-      title="Pizza"
-      text="Text about pizza"
-      price={10}
-    />
-    <Card
-      title="Ice Cream"
-      text="Text about ice cream"
-      price={3}
-    />
-    <Card
-      title="Chocolate"
-      text="Text about chocolate"
-      price={5}
-    />
-    <Card
-      title="Banana"
-      text="Text about banana"
-    />
+  <div
+    className="GoodList"
+  >
+    {goods.map((good) => (
+      <p
+        className="GoodCard"
+        style={{ color: good.color }}
+        key={good.id}
+      >
+        {good.name}
+      </p>
+    ))}
   </div>
 );
