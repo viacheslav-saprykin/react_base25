9f052755f4061e91e1698b3894c8894ee155b537
Viacheslav Saprykin <saprykinvp@gmail.com>
2025-02-24 19:47:06 +0200

    MA_Less1_ver.9 - useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
-import { GoodList } from "./components/GoodList";
-import goods from "./goods";
+// import { GoodList } from "./components/GoodList";
+// import goods from "./goods";
 
-export const App = () => (
-  <div className="App">
-    <GoodList goods={goods} />
-  </div>
-);
+import { useState } from "react";
+
+export const App = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div className="App">
+      {/* <GoodList goods={goods} /> */}
+      <h1>Value is {value} </h1>
+      <button
+        onClick={() => {
+          setValue(1);
+          console.log(value);
+        }}
+      >
+        1
+      </button>
+      <button
+        onClick={() => {
+          setValue(2);
+          console.log(value);
+        }}
+      >
+        2
+      </button>
+      <button
+        onClick={() => {
+          setValue(3);
+          console.log(value);
+        }}
+      >
+        3
+      </button>
+
+    </div>
+  );
+}
